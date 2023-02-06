50eece5bc558b243c15fb7432734e7fcc6dad37b
XandA <102875868+xandacompany@users.noreply.github.com>
2023-02-06 16:23:04 -0600

    Add files via upload

---
+++ Js/main.js
@@ -0,0 +1,19 @@
+function vinculo(){
+    location.href="https://ingasiel.github.io/";
+}
+
+function vinculo2(){
+    location.href="https://xandacompany.github.io/";
+}
+
+function ancla(){
+    location.href="#abajo";
+}
+
+function conocer(){
+    location.href="#contenido";
+}
+
+function vinculowhatsapp(){
+    location.href="https://wa.me/525536980694";
+}
